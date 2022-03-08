2a2baef16e1c84e82b5b9b5e623326dd90ceb9fb
Jaiesden123 <jaiesden@gmail.com>
2022-03-08 13:32:34 +0530

    Added New Class

---
+++ src/test/java/com/class1/Class2.java
@@ -0,0 +1,15 @@
+package com.class1;
+
+public class Class2 {
+	
+	public void method11() {
+		System.out.println("Java");
+
+	}
+	
+	public void method12() {
+		System.out.println("Selenium");
+
+	}
+
+}
